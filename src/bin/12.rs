b1ef8e590462ed8de8083297b4eb5300c27ca09d
Anton Andersson <antona@axis.com>
2025-12-12 15:25:58 +0100

    12
    Change-Id: I14dd7d618ee0aef51b4766806ea1def8a8ac1fa0

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,21 +1,16 @@
-fn impossible((w, h, v): &(usize, usize, Vec<usize>), shapes: &Vec<Vec<char>>) -> bool {
-    let n_fill: Vec<usize> = shapes
-        .iter()
-        .map(|v| v.iter().filter(|&c| *c == '#').count())
-        .collect();
-    let s: usize = v.iter().zip(n_fill.iter()).map(|(n1, n2)| n1 * n2).sum();
-
-    s > w * h
-}
-
 fn main() {
     let input: Vec<&str> = aoc::input(12).trim().split("\n\n").collect();
-    let shapes: Vec<Vec<char>> = input[..input.len() - 1]
+    let to_binary = |c| match c {
+        '#' => Some(1),
+        '.' => Some(0),
+        _ => None,
+    };
+    let shapes: Vec<Vec<usize>> = input[..input.len() - 1]
         .iter()
-        .map(|s| s.chars().filter(|&c| c == '#' || c == '.').collect())
+        .map(|s| s.chars().filter_map(|c| to_binary(c)).collect())
         .collect();
     let areas: Vec<(usize, usize, Vec<usize>)> = input[input.len() - 1]
-        .split("\n")
+        .lines()
         .map(|s| {
             let mut ns = s
                 .split(|c: char| !c.is_numeric())
@@ -25,16 +20,25 @@ fn main() {
         })
         .collect();
 
-    let my_shapes = vec![
-        "###\n###\n###\n###",                     // 3,3
-        "###\n###\n###\n###\n###\n###",           // 3,4,5
-        "###\n###\n###\n###\n###\n###\n###\n###", // 4,4,5,5
-    ];
+    let mut part1 = 0;
 
     let areas: Vec<(usize, usize, Vec<usize>)> = areas
         .into_iter()
-        .filter(|a| !impossible(a, &shapes))
+        .filter(|(w, h, ns)| {
+            let is_possible = w * h >= ns.iter().sum::<usize>() * 9;
+            part1 += if is_possible { 1 } else { 0 };
+            let n_optimal: usize = shapes
+                .iter()
+                .zip(ns.iter())
+                .map(|(s, n)| s.iter().sum::<usize>() * n)
+                .sum();
+            !is_possible || n_optimal <= w * h
+        })
         .collect();
 
-    println!("{:?} {:?}", areas.len(), shapes);
+    if areas.len() != 0 {
+        println!("{}", part1);
+    } else {
+        panic!("Crap! It's all you now...")
+    }
 }
